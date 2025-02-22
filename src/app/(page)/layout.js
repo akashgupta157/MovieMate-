47e831db79422ab290f10668fd1894e3a0a5d0ac
Akash Gupta <akashgupta989322@gmail.com>
2025-02-22 16:08:59 +0530

    refactor layout component to use useEffect for authentication and improve redirect logic

--- src/app/(page)/layout.js
+++ src/app/(page)/layout.js
@@ -1,25 +1,34 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import Sidebar from "@/components/Sidebar";
 import { CircleUser } from "lucide-react";
 
-export default function layout({ children }) {
-  const isAuth = localStorage.getItem("isLoggedIn");
-  if (isAuth) {
-    return (
-      <div className="p-5 h-screen flex gap-5">
-        <Sidebar />
-        {children}
-        <div className="absolute right-5 flex items-center space-x-2 font-bold">
-          <div className="bg-[#D9D9D9] p-2 rounded-full">
-            <CircleUser strokeWidth={2.5} />
-          </div>
-          <p>Naval Ravikant</p>
-        </div>
-      </div>
-    );
-  } else {
-    redirect("/login");
+export default function Layout({ children }) {
+  const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn) {
+      setIsAuth(true);
+    } else {
+      redirect("/login");
+    }
+  }, []);
+
+  if (!isAuth) {
+    return null; 
   }
+
+  return (
+    <div className="p-5 h-screen flex gap-5">
+      <Sidebar />
+      {children}
+      <div className="absolute right-5 flex items-center space-x-2 font-bold">
+        <div className="bg-[#D9D9D9] p-2 rounded-full">
+          <CircleUser strokeWidth={2.5} />
+        </div>
+        <p>Naval Ravikant</p>
+      </div>
+    </div>
+  );
 }
